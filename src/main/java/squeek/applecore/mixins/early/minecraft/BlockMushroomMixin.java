d47612d6a48deb21804a726806a7b96b17c26c28
インク <168640573+Xiaomo305Mua@users.noreply.github.com>
2026-09-08 00:32:51 +0800

    更新 BlockMushroomMixin.java

--- src/main/java/squeek/applecore/mixins/early/minecraft/BlockMushroomMixin.java
+++ src/main/java/squeek/applecore/mixins/early/minecraft/BlockMushroomMixin.java
@@ -7,41 +7,43 @@ import net.minecraft.block.BlockMushroom;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 
 import squeek.applecore.api.AppleCoreAPI;
 
 @Mixin(BlockMushroom.class)
 public class BlockMushroomMixin extends BlockBush {
 
+    @Unique
+    private boolean appleCore$executedCondition;
+
     @ModifyExpressionValue(
             method = "updateTick",
             at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 0))
-    private int onUpdateTick(int original, World worldIn, int x, int y, int z, Random random,
-            @Share("executedCondition") LocalBooleanRef executedCondition) {
+    private int onUpdateTick(int original, World worldIn, int x, int y, int z, Random random) {
         switch (AppleCoreAPI.dispatcher.validatePlantGrowth(this, worldIn, x, y, z, random)) {
             case ALLOW:
-                executedCondition.set(true);
+                this.appleCore$executedCondition = true;
                 return 0;
             case DEFAULT:
-                executedCondition.set(original == 0);
+                this.appleCore$executedCondition = original == 0;
                 return original;
             default: // DENY
+                this.appleCore$executedCondition = false;
                 return -1;
         }
     }
 
     @Inject(method = "updateTick", at = @At("RETURN"))
-    private void afterUpdateTick(World worldIn, int x, int y, int z, Random random, CallbackInfo ci,
-            @Share("executedCondition") LocalBooleanRef executedCondition) {
-        if (executedCondition.get()) {
+    private void afterUpdateTick(World worldIn, int x, int y, int z, Random random, CallbackInfo ci) {
+        if (this.appleCore$executedCondition) {
             AppleCoreAPI.dispatcher.announcePlantGrowthWithoutMetadataChange(this, worldIn, x, y, z);
         }
+        this.appleCore$executedCondition = false;
     }
 }
