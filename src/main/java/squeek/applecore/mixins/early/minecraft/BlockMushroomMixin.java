43b2e68ce09e3e3a617d4845288b21b70c6f04a1
インク <168640573+Xiaomo305Mua@users.noreply.github.com>
2026-09-08 01:03:28 +0800

    更新 BlockMushroomMixin.java

--- src/main/java/squeek/applecore/mixins/early/minecraft/BlockMushroomMixin.java
+++ src/main/java/squeek/applecore/mixins/early/minecraft/BlockMushroomMixin.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import squeek.applecore.api.AppleCoreAPI;
+import squeek.applecore.api.plants.PlantGrowthResult;
 
 @Mixin(BlockMushroom.class)
 public class BlockMushroomMixin extends BlockBush {
@@ -26,16 +27,16 @@ public class BlockMushroomMixin extends BlockBush {
             method = "updateTick",
             at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 0))
     private int onUpdateTick(int original, World worldIn, int x, int y, int z, Random random) {
-        switch (AppleCoreAPI.dispatcher.validatePlantGrowth(this, worldIn, x, y, z, random)) {
-            case ALLOW:
-                this.appleCore$executedCondition = true;
-                return 0;
-            case DEFAULT:
-                this.appleCore$executedCondition = original == 0;
-                return original;
-            default: // DENY
-                this.appleCore$executedCondition = false;
-                return -1;
+        PlantGrowthResult result = AppleCoreAPI.dispatcher.validatePlantGrowth(this, worldIn, x, y, z, random);
+        if (result == PlantGrowthResult.ALLOW) {
+            this.appleCore$executedCondition = true;
+            return 0;
+        } else if (result == PlantGrowthResult.DEFAULT) {
+            this.appleCore$executedCondition = original == 0;
+            return original;
+        } else { // DENY
+            this.appleCore$executedCondition = false;
+            return -1;
         }
     }
 
